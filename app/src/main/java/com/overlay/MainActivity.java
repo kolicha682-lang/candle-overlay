ad570264ded4d04d2a1be59bbc9f1a9c71e577aa
kolicha682-lang <kolicha682@gmail.com>
2025-12-04 09:28:07 +0000

    Update MainActivity.java

--- app/src/main/java/com/overlay/MainActivity.java
+++ app/src/main/java/com/overlay/MainActivity.java
@@ -1,1 +1,52 @@
+package com.overlay;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_OVERLAY = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // طلب إذن الظهور فوق التطبيقات
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName())
+                );
+                startActivityForResult(intent, REQUEST_OVERLAY);
+                return;
+            }
+        }
+
+        startOverlayService();
+        finish(); // إغلاق الواجهة لأننا لا نحتاجها
+    }
+
+    private void startOverlayService() {
+        Intent intent = new Intent(this, OverlayService.class);
+        startService(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_OVERLAY) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+                Settings.canDrawOverlays(this)) {
+
+                startOverlayService();
+                finish();
+            }
+        }
+    }
+}
