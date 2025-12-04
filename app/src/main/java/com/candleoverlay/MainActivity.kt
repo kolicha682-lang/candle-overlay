9b777e2594d3bbd767b2fa5e91ae88f78567af56
kolicha682-lang <kolicha682@gmail.com>
2025-12-04 09:12:31 +0000

    Update MainActivity.kt

--- app/src/main/java/com/candleoverlay/MainActivity.kt
+++ app/src/main/java/com/candleoverlay/MainActivity.kt
@@ -1,1 +1,21 @@
+package com.example.candleoverlay
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val web = findViewById<WebView>(R.id.web)
+        web.settings.javaScriptEnabled = true
+        web.settings.domStorageEnabled = true
+        web.webViewClient = WebViewClient()
+
+        // رابط منصة كوتاكس
+        web.loadUrl("https://quotex.com")
+    }
+}
